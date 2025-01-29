8e992fbda1f89201cdcd96902e484bcca13424e1
zach-blumenfeld <zach.blumenfeld@neo4j.com>
2025-01-29 10:01:21 -0500

    Fixing Retail Customer Segmentation SQL Files and Adding Notebook for Analysis

--- retail/retail-step1-customer-segmentation.sql
+++ retail/retail-step1-customer-segmentation.sql
@@ -1,36 +1,85 @@
+-- ///////////////////////////////////////////////////////////
+-- 1) SETUP
+-- ///////////////////////////////////////////////////////////
+-- Create a consumer role for users of the GDS application
+CREATE ROLE IF NOT EXISTS gds_role;
+GRANT APPLICATION ROLE neo4j_graph_analytics.app_user TO ROLE gds_role;
+-- Create a consumer role for administrators of the GDS application
+CREATE ROLE IF NOT EXISTS gds_admin_role;
+GRANT APPLICATION ROLE neo4j_graph_analytics.app_admin TO ROLE gds_admin_role;
+
+-- Grant access to data
+-- The application reads data to build a graph object, and it also writes results into new tables.
+-- We therefore need to grant the right permissions to give the application access.
+GRANT USAGE ON DATABASE RETAIL TO APPLICATION neo4j_graph_analytics;
+GRANT USAGE ON SCHEMA RETAIL.PUBLIC TO APPLICATION neo4j_graph_analytics;
+
+-- required to read view data into a graph
+GRANT SELECT ON ALL VIEWS IN SCHEMA RETAIL.PUBLIC TO APPLICATION neo4j_graph_analytics;
+GRANT SELECT ON ALL TABLES IN SCHEMA RETAIL.PUBLIC TO APPLICATION neo4j_graph_analytics;
+-- required to write computation results into a table
+GRANT CREATE TABLE ON SCHEMA RETAIL.PUBLIC TO APPLICATION neo4j_graph_analytics;
+-- optional, ensuring the consumer role has access to tables created by the application
+GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA RETAIL.PUBLIC TO ROLE gds_role;
+GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA RETAIL.PUBLIC TO ROLE accountadmin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA RETAIL.PUBLIC TO ROLE accountadmin;
+
+-- Spin up Neo4j
+CALL neo4j_graph_analytics.gds.create_session('CPU_X64_M');
+
+-- show the various functions available
+SHOW USER FUNCTIONS IN APPLICATION neo4j_graph_analytics;
+
+
+-- ///////////////////////////////////////////////////////////
+-- 2) graph algorithms for segmentation
+-- ///////////////////////////////////////////////////////////
 USE DATABASE RETAIL;
 USE SCHEMA RETAIL.PUBLIC;
 
--- create node mapping
-CREATE OR REPLACE SEQUENCE node_id START = 0 INCREMENT = 1;
+-- drop graph if already exists.
+SELECT neo4j_graph_analytics.gds.graph_drop('customer_graph', { 'failIfMissing': false });
 
-CREATE OR REPLACE TABLE CUSTOMER_NODE_MAPPING (NODEID, CUSTOMERID) AS
-SELECT node_id.nextval, CUSTOMERID
-FROM CUSTOMERS;
-SELECT * FROM CUSTOMER_NODE_MAPPING;
+-- create graph projection
+SELECT neo4j_graph_analytics.gds.graph_project('customer_graph', {
+    'nodeTable': 'retail.public.customer_nodes',
+                                               'relationshipTable': 'retail.public.customer_relationships',
+                                               'readConcurrency': 28
+           });
 
--- final node view to use with Neo4j
-CREATE OR REPLACE VIEW CUSTOMER_NODES(nodeId) AS
-SELECT NODEID FROM CUSTOMER_NODE_MAPPING;
-SELECT * FROM CUSTOMER_NODES;
+-- run louvain community detection
+SELECT neo4j_graph_analytics.gds.louvain('customer_graph', {
+    'relationshipWeightProperty': 'CO_PURCHASE_COUNT',
+                                         'mutateProperty': 'customer_segment'
+           });
 
--- relationship table
-CREATE OR REPLACE VIEW CUSTOMER_RELATIONSHIPS(sourceNodeId, targetNodeId, co_purchase_count) AS
-SELECT
-    src_nodes.NODEID AS sourceNodeId,
-    tgt_nodes.NODEID AS targetNodeId,
-    TO_DOUBLE(COUNT(*))
-FROM TRANSACTIONS AS t1
-         FULL OUTER JOIN TRANSACTIONS AS t2
-                         ON t1.articleId=t2.articleId
-         INNER JOIN CUSTOMER_NODE_MAPPING AS src_nodes
-                    ON t1.customerId = src_nodes.customerId
-         INNER JOIN CUSTOMER_NODE_MAPPING  AS tgt_nodes
-                    ON t2.customerId = tgt_nodes.customerId
-WHERE t1.customerId <> t2.customerId
-GROUP BY sourceNodeId, targetNodeId, t1.articleId;
+-- write back to table
+SELECT neo4j_graph_analytics.gds.write_nodeproperties('customer_graph',
+           {'nodeProperties': ['customer_segment'], 'table': 'retail.public.customer_metrics'});
+SELECT * FROM CUSTOMER_METRICS ORDER BY customer_segment;
 
-SELECT count(*) FROM CUSTOMER_RELATIONSHIPS;
-SELECT * FROM CUSTOMER_RELATIONSHIPS ORDER BY co_purchase_count DESC;
+-- create customer purchase by segment view for downstream analytics
+SELECT *
+FROM customers
+         JOIN customer_node_mapping ON customer_node_mapping.customerId=customers.customerId
+         JOIN customer_metrics ON customer_node_mapping.nodeId=customer_metrics.nodeId
+ORDER BY customer_metrics.customer_segment DESC
 
+CREATE OR REPLACE VIEW CUSTOMER_PURCHASES AS
+SELECT customer_metrics.customer_segment,
+       transactions.*,
+       articles.prodname,
+       articles.producttypename
+FROM customers
+         JOIN customer_node_mapping ON customer_node_mapping.customerId=customers.customerId
+         JOIN customer_metrics ON customer_node_mapping.nodeId=customer_metrics.nodeId
+         JOIN transactions ON transactions.customerId = customers.customerId
+         JOIN articles ON articles.articleId = transactions.articleId
+ORDER BY customer_metrics.customer_segment DESC;
+SELECT * FROM CUSTOMER_PURCHASES;
 
+-- ///////////////////////////////////////////////////////////
+-- 3) CLEANUP NEO4J
+-- ///////////////////////////////////////////////////////////
+-- turn off Neo4j and spins down compute pool
+CALL neo4j_graph_analytics.gds.stop_session();
