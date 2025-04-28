58a8f2d6c33c9e105f3decdd1df4bbaffa68a249
Olga Razvenskaia <olga.razvenskaia@neo4j.com>
2025-04-28 22:30:38 +0100

    Use avalanche API for gs example

--- genre-recommendation/README.md
+++ genre-recommendation/README.md
@@ -6,11 +6,11 @@ The dataset has been processed to be compatible with the Snowflake application `
 The graph contains node labels 'ACTOR', 'MOVIE' and 'DIRECTOR' and relationship types 'ACTED_IN' and 'DIRECTED_IN'.
 All nodes contain 1256 dimensional feature vectors via the property "plot_keywords". Some of the MOVIE nodes have a 'GENRE' value of 0, 1 or 2.
 
-A method is provided in `imdb/load_to_pandas.py` to load the dataset into dataframes describing nodes and relationships for the IMDB graph.
+A method is provided in `genre-recommendation/load_to_pandas.py` to load the dataset into dataframes describing nodes and relationships for the IMDB graph.
 
 ## Ingesting the IMDB dataset into Snowflake
 
-A script `imdb/upload_imdb_to_snowflake.py` uploads the data into a snowflake account.
+A script `genre-recommendation/upload_imdb_to_snowflake.py` uploads the data into a snowflake account.
 For usage of this script run `python upload_imdb_to_snowflake.py` from the imdb folder.
 The script takes command line arguments about snowflake configuration. 
 The snowflake host depends on region but can be for example, `snowflake.eu-west-2.aws.snowflakecomputing.com`.
--- genre-recommendation/graph_sage.sql
+++ genre-recommendation/graph_sage.sql
@@ -1,29 +1,116 @@
-USE ROLE <A_ROLE>;
+USE ROLE ACCOUNTADMIN;
 
-GRANT USAGE ON DATABASE <A_DATABASE> TO APPLICATION <APP_NAME>;
-GRANT USAGE ON SCHEMA <A_DATABASE>.<A_SCHEMA> TO APPLICATION <APP_NAME>;
-GRANT CREATE STAGE ON SCHEMA <A_DATABASE>.<A_SCHEMA> TO APPLICATION <APP_NAME>;
-GRANT SELECT ON ALL TABLES IN SCHEMA <A_DATABASE>.<A_SCHEMA> TO APPLICATION <APP_NAME>;
-GRANT CREATE TABLE ON SCHEMA <A_DATABASE>.<A_SCHEMA> TO APPLICATION <APP_NAME>;
-GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA <A_DATABASE>.<A_SCHEMA> TO ROLE <A_ROLE>;
-USE DATABASE <APP_NAME>;
-USE SCHEMA gml;
+-- Let's assume you have a database named 'genre_classification_db' which contains data in the schema 'imdb'
+-- and 'results' for storing the results of algorithm predictions.
+-- Application name is Neo4j_GDS
+GRANT USAGE ON DATABASE genre_classification_db TO APPLICATION Neo4j_GDS;
+GRANT USAGE ON SCHEMA genre_classification_db.results TO APPLICATION Neo4j_GDS;
+GRANT USAGE ON SCHEMA genre_classification_db.imdb TO APPLICATION Neo4j_GDS;
+
+GRANT CREATE STAGE ON SCHEMA genre_classification_db.results TO APPLICATION Neo4j_GDS;
+GRANT CREATE STAGE ON SCHEMA genre_classification_db.imdb TO APPLICATION Neo4j_GDS;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA genre_classification_db.results TO APPLICATION Neo4j_GDS;
+GRANT SELECT ON ALL TABLES IN SCHEMA genre_classification_db.imdb TO APPLICATION Neo4j_GDS;
+
+GRANT CREATE TABLE ON SCHEMA genre_classification_db.results TO APPLICATION Neo4j_GDS;
+GRANT CREATE TABLE ON SCHEMA genre_classification_db.imdb TO APPLICATION Neo4j_GDS;
+
+GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA genre_classification_db.results TO ROLE accountadmin;
+
+-- `gds_role` is a consumer role for users of the application.
+-- CREATE ROLE IF NOT EXISTS gds_role;
+-- GRANT APPLICATION ROLE NEO4J_GRAPH_ANALYTICS.app_user TO ROLE gds_role;
+
+GRANT USAGE, MONITOR ON DATABASE genre_classification_db TO role gds_role;
+GRANT USAGE, MONITOR ON schema genre_classification_db.imdb TO role gds_role;
+GRANT USAGE, MONITOR ON schema genre_classification_db.results TO role gds_role;
+GRANT SELECT ON ALL TABLES IN SCHEMA genre_classification_db.results TO role gds_role;
+GRANT SELECT ON ALL TABLES IN SCHEMA genre_classification_db.imdb TO role gds_role;
+GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA genre_classification_db.results TO ROLE gds_role;
+GRANT ALL PRIVILEGES ON FUTURE TABLES IN SCHEMA genre_classification_db.imdb TO ROLE gds_role;
+
+USE ROLE gds_role;
+
+USE DATABASE genre_classification_db;
+USE SCHEMA results;
 USE WAREHOUSE <A_WAREHOUSE>;
 
-CALL gml.create_session('GPU_NV_S');
+-- Training stage of the GraphSAGE node classification algorithm
+CALL Neo4j_GDS.graph.gs_nc_train('GPU_NV_S', {
+    'graph_config': {
+        'default_table_prefix': 'genre_classification_db.imdb',
+        'node_tables': ['actor', 'director', 'movie'],
+        'relationship_tables': {
+            'acted_in': {'source_table': 'actor', 'target_table': 'movie', 'orientation': 'UNDIRECTED'},
+            'directed_in': {'source_table': 'director', 'target_table': 'movie', 'orientation': 'UNDIRECTED'}
+        }
+    },
+    'task_config': {
+        'modelname': 'nc-imdb',
+        'num_epochs': 10,
+        'num_samples': [20, 20],
+        'target_label': 'movie',
+        'target_property': 'genre',
+        'class_weights': true
+    }
+});
 
-SELECT gml.gs_nc_train({'modelname': 'nc-imdb', 'graph_config': {'database': '<A_DATABASE>', 'schema': '<A_SCHEMA>', 'node_tables': {'actor': ['plot_keywords'], 'director': ['plot_keywords'], 'movie': ['plot_keywords', 'genre']}, 'rel_tables': {'acted_in': [], 'directed_in': []}}, 'task_config': {'num_epochs': 10, 'num_samples': [20, 20], 'target_label': 'movie', 'target_property': 'genre', 'class_weights': true}});
+-- Prediction stage of the GraphSAGE node classification algorithm
+CALL Neo4j_GDS.graph.gs_nc_predict('GPU_NV_S', {
+    'graph_config': {
+        'default_table_prefix': 'genre_classification_db.imdb',
+        'node_tables': ['actor', 'director', 'movie'],
+        'relationship_tables': {
+            'acted_in': {'source_table': 'actor', 'target_table': 'movie', 'orientation': 'UNDIRECTED'},
+            'directed_in': {'source_table': 'director', 'target_table': 'movie', 'orientation': 'UNDIRECTED'}
+        }
+    },
+    'task_config': {
+        'modelname': 'nc-imdb'
+    },
+    'output_config': [{'node_label': 'movie', 'output_table': 'genre_classification_db.results.genre_predictions'}]
+});
 
-SELECT gml.gs_nc_predict({'modelname': 'nc-imdb', 'graph_config': { 'node_output_table': 'genre_predictions', 'database': '<A_DATABASE>', 'schema': '<A_SCHEMA>', 'node_tables': {'actor': ['plot_keywords'], 'director': ['plot_keywords'], 'movie': ['plot_keywords', 'genre']}, 'rel_tables': {'acted_in': [], 'directed_in': []}}, 'task_config': {}});
+-- Check the results of the predictions
+SELECT * FROM genre_classification_db.results.genre_predictions LIMIT 20;
 
-SELECT * FROM <A_DATABASE>.<A_SCHEMA>.genre_predictions LIMIT 20;
+-- Training stage of the GraphSAGE unsupervised algorithm
+CALL Neo4j_GDS.graph.gs_unsup_train('GPU_NV_S', {
+    'graph_config': {
+        'default_table_prefix': 'genre_classification_db.imdb',
+        'node_tables': ['actor', 'director', 'movie'],
+        'relationship_tables': {
+            'acted_in': {'source_table': 'actor', 'target_table': 'movie', 'orientation': 'UNDIRECTED'},
+            'directed_in': {'source_table': 'director', 'target_table': 'movie', 'orientation': 'UNDIRECTED'}
+        }
+    },
+    'task_config': {
+        'modelname': 'unsup-imdb',
+        'num_epochs': 10,
+        'num_samples': [20, 20]
+    }
+});
 
--- The below uses unsupervised graphsage to train a model and generate node embeddings
+-- Prediction stage of the GraphSAGE unsupervised algorithm - computing embeddings
+CALL Neo4j_GDS.graph.gs_unsup_predict('GPU_NV_S', {
+    'modelname': 'unsup-imdb',
+    'graph_config': {
+        'default_table_prefix': 'genre_classification_db.imdb',
+        'node_tables': ['actor', 'director', 'movie'],
+        'relationship_tables': {
+            'acted_in': {'source_table': 'actor', 'target_table': 'movie', 'orientation': 'UNDIRECTED'},
+            'directed_in': {'source_table': 'director', 'target_table': 'movie', 'orientation': 'UNDIRECTED'}
+        }
+    },
+    'task_config': {
+        'modelname': 'unsup-imdb'
+    },
+    'output_config': [{
+        'node_label': 'movie',
+        'output_table': 'genre_classification_db.results.movie_embeddings'
+    }]
+});
 
-
-SELECT gml.gs_unsup_train({'modelname': 'unsup-imdb', 'graph_config': {'database': '<A_DATABASE>', 'schema': '<A_SCHEMA>', 'node_tables': {'actor': ['plot_keywords'], 'director': ['plot_keywords'], 'movie': ['plot_keywords']}, 'rel_tables': {'acted_in': [], 'directed_in': []}}, 'task_config': {'num_epochs': 10, 'num_samples': [20, 20]}});
-
-SELECT GML.gs_unsup_predict({'modelname': 'unsup-imdb', 'graph_config': { 'node_output_table': 'imdb_embeddings', 'database': '<A_DATABASE>', 'schema': '<A_SCHEMA>', 'node_tables': {'actor': ['plot_keywords'], 'director': ['plot_keywords'], 'movie': ['plot_keywords']}, 'rel_tables': {'acted_in': [], 'directed_in': []}}, 'task_config': {}});
-
-
-SELECT * FROM <A_DATABASE>.<A_SCHEMA>.imdb_embeddings LIMIT 20;
+-- Check the embeddings
+SELECT * FROM genre_classification_db.results.movie_embeddings LIMIT 20;
